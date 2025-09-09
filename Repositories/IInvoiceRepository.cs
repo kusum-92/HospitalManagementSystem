85804d60240bae706c65d47e5c3cf8c110756e03
akku83344 <akku83344@gmail.com>
2025-09-09 11:31:23 +0530

    Invoice repository fix

--- Repositories/IInvoiceRepository.cs
+++ Repositories/IInvoiceRepository.cs
@@ -1,6 +1,14 @@
-﻿namespace HospitalManagementSystem.Repositories
+﻿using HospitalManagementSystem.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HospitalManagementSystem.Repository.Interfaces
 {
-    public class IInvoiceRepository
+    public interface IInvoiceRepository
     {
+        Task<IEnumerable<Invoice>> GetAllAsync();
+        Task<Invoice?> GetByIdAsync(int id);
+        Task AddAsync(Invoice invoice);
+        Task SaveAsync();
     }
 }
--- Repositories/InvoiceRepository.cs
+++ Repositories/InvoiceRepository.cs
@@ -1,6 +1,45 @@
-﻿namespace HospitalManagementSystem.Repositories
+﻿using HospitalManagementSystem.Data;
+using HospitalManagementSystem.Models;
+using HospitalManagementSystem.Repository.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HospitalManagementSystem.Repository.Repositories
 {
-    public class InvoiceRepository
+    public class InvoiceRepository : IInvoiceRepository
     {
+        private readonly AppDbContext _context;
+
+        public InvoiceRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Invoice>> GetAllAsync()
+        {
+            return await _context.Invoices
+                .Include(i => i.Patient)
+                .Include(i => i.Appointment)
+                .ToListAsync();
+        }
+
+        public async Task<Invoice?> GetByIdAsync(int id)
+        {
+            return await _context.Invoices
+                .Include(i => i.Patient)
+                .Include(i => i.Appointment)
+                .FirstOrDefaultAsync(i => i.InvoiceId == id);
+        }
+
+        public async Task AddAsync(Invoice invoice)
+        {
+            await _context.Invoices.AddAsync(invoice);
+        }
+
+        public async Task SaveAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
     }
 }
