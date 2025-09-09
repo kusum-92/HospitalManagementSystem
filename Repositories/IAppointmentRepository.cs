8409c099395d43e249a57d962761818d2c532ceb
akku83344 <akku83344@gmail.com>
2025-09-09 11:24:02 +0530

    Repositories

---
+++ Repositories/IAppointmentRepository.cs
@@ -0,0 +1,14 @@
+﻿using HospitalManagementSystem.Models;
+
+namespace HospitalManagementSystem.Repository.Interfaces
+{
+    public interface IAppointmentRepository
+    {
+        Task<IEnumerable<Appointment>> GetAllAsync();
+        Task<Appointment?> GetByIdAsync(int id);
+        Task AddAsync(Appointment appointment);
+        void Update(Appointment appointment);
+        void Delete(Appointment appointment);
+        Task SaveAsync();
+    }
+}
---
+++ Repositories/IPatientRepository.cs
@@ -0,0 +1,16 @@
+﻿using HospitalManagementSystem.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HospitalManagementSystem.Repository.Interfaces
+{
+    public interface IPatientRepository
+    {
+        Task<IEnumerable<Patient>> GetAllAsync();
+        Task<Patient?> GetByIdAsync(int id);
+        Task AddAsync(Patient patient);
+        void Update(Patient patient);
+        void Delete(Patient patient);
+        Task SaveAsync();
+    }
+}
